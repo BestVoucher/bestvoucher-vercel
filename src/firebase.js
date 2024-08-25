aa699b689a95736c31105bac7660b4800c791943
Matteo Pinzauti <pinza99@gmail.com>
2024-08-25 11:49:27 +0200

    Moved Firebase config to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -5,16 +5,15 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBQgp-5paXLfntSRuWnPNLq2arWEZTxwPU",
-  authDomain: "voucher-bf9ef.firebaseapp.com",
-  projectId: "voucher-bf9ef",
-  storageBucket: "voucher-bf9ef.appspot.com",
-  messagingSenderId: "814142702634",
-  appId: "1:814142702634:web:94b9c13a01125d2abc6f53",
-  measurementId: "G-SSYCK730XH"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
