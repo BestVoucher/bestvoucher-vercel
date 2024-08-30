0afe2663933f6f7a2b195de000271f9416c55714
Matteo Pinzauti <pinza99@gmail.com>
2024-08-30 17:47:40 +0200

    Navbar aggiornata

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate, useLocation } from 'react-router-dom';  // Aggiungi useLocation per ottenere il percorso corrente
 import { useAuth } from '../context/AuthContext';
 import { auth } from '../firebase';
 import { signOut } from 'firebase/auth';
@@ -9,15 +9,30 @@ import logo from '../assets/logo.png';
 function Navbar() {
   const { currentUser, userData } = useAuth();
   const navigate = useNavigate();
+  const location = useLocation();  // Ottieni la posizione corrente
   const [isMobile, setIsMobile] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
+
+  // Mappa dei percorsi ai nomi delle pagine
+  const pageTitles = {
+    '/': 'Home',
+    '/personal-area': 'Profilo',
+    '/admin-dashboard': 'Dashboard Admin',
+    '/sell-product': 'Vendi',
+    '/received-orders': 'Ordini ricevuti',
+    '/orders': 'Ordini',
+    '/login': 'Login',
+    '/register': 'Registrati',
+    '/terms': 'Termini e Condizioni',
+    // Aggiungi altri percorsi qui se necessario
+  };
 
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768);
     };
 
-    handleResize(); // Check the initial size
+    handleResize(); // Verifica la dimensione iniziale
     window.addEventListener('resize', handleResize);
 
     return () => {
@@ -38,6 +53,8 @@ function Navbar() {
     setMenuOpen(!menuOpen);
   };
 
+  const currentPageTitle = pageTitles[location.pathname] || '';  // Ottieni il titolo della pagina corrente
+
   return (
     <nav className="navbar">
       <div className="navbar-logo-container">
@@ -46,83 +63,70 @@ function Navbar() {
         </NavLink>
       </div>
 
+      {isMobile && (
+        <div className="navbar-page-title">
+          {currentPageTitle}
+        </div>
+      )}
+
       {isMobile ? (
         <div className="navbar-mobile-menu">
           <button className="menu-toggle-button" onClick={toggleMenu}>
             ☰
           </button>
-          <ul className={`navbar-mobile-dropdown ${menuOpen ? 'open' : ''}`}>
-            <li className="navbar-item">
-              <NavLink to="/" onClick={toggleMenu} activeClassName="active">Home</NavLink>
-            </li>
-            <li className="navbar-item">
-              <NavLink to="/personal-area" onClick={toggleMenu} activeClassName="active">Profilo</NavLink>
-            </li>
-            {userData?.role === 'admin' && (
-              <li className="navbar-item">
-                <NavLink to="/admin-dashboard" onClick={toggleMenu} activeClassName="active">Dashboard Admin</NavLink>
-              </li>
-            )}
-            {userData?.role === 'company' && userData?.status === 'approved' && (
-              <>
-                <li className="navbar-item">
-                  <NavLink to="/sell-product" onClick={toggleMenu} activeClassName="active">Vendi</NavLink>
-                </li>
-                <li className="navbar-item">
-                  <NavLink to="/received-orders" onClick={toggleMenu} activeClassName="active">Ordini ricevuti</NavLink>
-                </li>
-              </>
-            )}
-            {userData?.role === 'user' && (
-              <li className="navbar-item">
-                <NavLink to="/orders" onClick={toggleMenu} activeClassName="active">Ordini</NavLink>
-              </li>
-            )}
-            {!currentUser && (
-              <li className="navbar-item">
-                <NavLink to="/login" onClick={toggleMenu} activeClassName="active">Login</NavLink>
-              </li>
-            )}
-            {currentUser && (
-              <li className="navbar-item">
-                <button onClick={() => { handleLogout(); toggleMenu(); }} className="logout-button">Logout</button>
-              </li>
-            )}
-          </ul>
+          {menuOpen && (
+            <ul className="navbar-mobile-dropdown">
+              <li className="navbar-item"><NavLink to="/" onClick={toggleMenu} activeClassName="active">Home</NavLink></li>
+              <li className="navbar-item"><NavLink to="/personal-area" onClick={toggleMenu} activeClassName="active">Profilo</NavLink></li>
+              
+              {userData?.role === 'admin' && (
+                <li className="navbar-item"><NavLink to="/admin-dashboard" onClick={toggleMenu} activeClassName="active">Dashboard Admin</NavLink></li>
+              )}
+
+              {userData?.role === 'company' && userData?.status === 'approved' && (
+                <>
+                  <li className="navbar-item"><NavLink to="/sell-product" onClick={toggleMenu} activeClassName="active">Vendi</NavLink></li>
+                  <li className="navbar-item"><NavLink to="/received-orders" onClick={toggleMenu} activeClassName="active">Ordini ricevuti</NavLink></li>
+                </>
+              )}
+
+              {userData?.role === 'user' && (
+                <li className="navbar-item"><NavLink to="/orders" onClick={toggleMenu} activeClassName="active">Ordini</NavLink></li>
+              )}
+
+              {!currentUser && (
+                <li className="navbar-item"><NavLink to="/login" onClick={toggleMenu} activeClassName="active">Login</NavLink></li>
+              )}
+
+              {currentUser && (
+                <li className="navbar-item"><button onClick={() => { handleLogout(); toggleMenu(); }} className="logout-button">Logout</button></li>
+              )}
+            </ul>
+          )}
         </div>
       ) : (
         <div className="navbar-menu-container">
           <ul className="navbar-menu">
-            <li className="navbar-item">
-              <NavLink to="/" activeClassName="active">Home</NavLink>
-            </li>
-            <li className="navbar-item">
-              <NavLink to="/personal-area" activeClassName="active">Profilo</NavLink>
-            </li>
+            <li className="navbar-item"><NavLink to="/" activeClassName="active">Home</NavLink></li>
+            <li className="navbar-item"><NavLink to="/personal-area" activeClassName="active">Profilo</NavLink></li>
+            
             {userData?.role === 'admin' && (
-              <li className="navbar-item">
-                <NavLink to="/admin-dashboard" activeClassName="active">Dashboard Admin</NavLink>
-              </li>
+              <li className="navbar-item"><NavLink to="/admin-dashboard" activeClassName="active">Dashboard Admin</NavLink></li>
             )}
+
             {userData?.role === 'company' && userData?.status === 'approved' && (
               <>
-                <li className="navbar-item">
-                  <NavLink to="/sell-product" activeClassName="active">Vendi</NavLink>
-                </li>
-                <li className="navbar-item">
-                  <NavLink to="/received-orders" activeClassName="active">Ordini ricevuti</NavLink>
-                </li>
+                <li className="navbar-item"><NavLink to="/sell-product" activeClassName="active">Vendi</NavLink></li>
+                <li className="navbar-item"><NavLink to="/received-orders" activeClassName="active">Ordini ricevuti</NavLink></li>
               </>
             )}
+
             {userData?.role === 'user' && (
-              <li className="navbar-item">
-                <NavLink to="/orders" activeClassName="active">Ordini</NavLink>
-              </li>
+              <li className="navbar-item"><NavLink to="/orders" activeClassName="active">Ordini</NavLink></li>
             )}
+
             {!currentUser && (
-              <li className="navbar-item">
-                <NavLink to="/login" activeClassName="active">Login</NavLink>
-              </li>
+              <li className="navbar-item"><NavLink to="/login" activeClassName="active">Login</NavLink></li>
             )}
           </ul>
         </div>
